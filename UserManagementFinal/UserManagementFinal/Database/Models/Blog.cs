868316df711640e9b8caee2f1b5aa6c87c621708
KamilMammadov <kamiltm@code.edu.az>
2022-08-09 16:24:54 +0400

    blog model properties written

--- UserManagementFinal/UserManagementFinal/Database/Models/Blog.cs
+++ UserManagementFinal/UserManagementFinal/Database/Models/Blog.cs
@@ -8,7 +8,19 @@ namespace UserManagementFinal.Database.Models
 {
     class Blog
     {
+        public string Tittle { get; set; }
+        public string Content { get; set; }
+        public int ID { get; set; }
+        public DateTime CreadetTime { get; set; }
+        public List<Comments> Comments { get; set; }
 
+        public Blog(string tittle,string content,List<Comments> comments)
+        {
+            Tittle = tittle;
+            Content = content;
+            Comments = comments;
+
+        }
 
     }
 }
