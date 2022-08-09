236244eb1f49e56271e22ec0a3db0dddf3c92868
KamilMammadov <kamiltm@code.edu.az>
2022-08-09 16:31:18 +0400

    Random ID added to blog

--- UserManagementFinal/UserManagementFinal/Database/Models/Blog.cs
+++ UserManagementFinal/UserManagementFinal/Database/Models/Blog.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagementFinal.Database.Models.Common;
+using UserManagementFinal.Database.Models.Repository;
 
 namespace UserManagementFinal.Database.Models
 {
-    class Blog
+    class Blog: Entity<string>
     {
         public string Tittle { get; set; }
         public string Content { get; set; }
-        public int ID { get; set; }
+        public string ID { get; set; }
         public DateTime CreadetTime { get; set; }
         public List<Comments> Comments { get; set; }
 
-        public Blog(string tittle,string content,List<Comments> comments)
+        public Blog(string tittle,string content,List<Comments> comments,string id=null)
         {
             Tittle = tittle;
             Content = content;
             Comments = comments;
+            if (id!=null)
+            {
+                ID = id;
+            }
+            else
+            {
+                ID = BlogRepository.RandomCode;
+            }
 
         }
 
--- UserManagementFinal/UserManagementFinal/Database/Models/Repository/BlogRepository.cs
+++ UserManagementFinal/UserManagementFinal/Database/Models/Repository/BlogRepository.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagementFinal.Database.Models.Repository.Common;
 
 namespace UserManagementFinal.Database.Models.Repository
 {
-    class BlogRepository
+     class BlogRepository : Repository<Blog,string>
     {
+        static Random randomID = new Random();
+
+        private static string _code;
+
+
+        public static string RandomCode
+        {
+            get
+            {
+                _code = "BL" + randomID.Next();
+                return _code;
+            }
+
+        }
     }
 }
