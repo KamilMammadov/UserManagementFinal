078e5065aa43dbf9e2ae88fedacaef9a4c9a0bbb
KamilMammadov <kamiltm@code.edu.az>
2022-08-10 13:44:35 +0400

    Inbox model's properties written

--- UserManagementFinal/UserManagementFinal/Database/Models/Inbox.cs
+++ UserManagementFinal/UserManagementFinal/Database/Models/Inbox.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagementFinal.Database.Models.Common;
 
 namespace UserManagementFinal.Database.Models
 {
-    class Inbox
+    class Inbox : Entity<int>
     {
+        public User To { get; set; }
+        public Blog Blog { get; set; }
+        public string Message { get; set; }
+        public Inbox(User to, Blog blog, string message)
+        {
+            To = to;
+            Blog = blog;
+            Message = message;
+        }
+
+        public string GetMessage()
+        {
+            return Blog.ID + " " + Message;
+        }
     }
 }
